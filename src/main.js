8975f52e1fbc566ce42b4e8b05754a7294281851
Merimer08 <marialolu08@gmail.com>
2025-03-08 18:33:30 +0100

    integrar html a main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+/* import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from './counter.js'*/
 
 document.querySelector('#app').innerHTML = `
+
+  <main>
+    <!-- Título principal -->
+    <h1>Buscador de Libros</h1>
+    <!-- Formulario de búsqueda -->
+    <form class="search-form" id="searchForm">
+      <input type="text" id="searchInput" class="search-input" placeholder="Ingresa el título del libro">
+      <button type="submit" class="search-button">Buscar</button>
+    </form>
+    <!-- Contenedor para mensajes de error -->
+    <div id="errorMessage" class="error-message"></div>
+    <!-- Contenedor para mostrar los resultados de la búsqueda -->
+    <div id="resultsContainer" class="results-container"></div>
+
+    <!-- Script principal de la aplicación -->
+    <script type="module" src="/src/main.js"></script>
+  </main>
+`;
+
+
+
+/* `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +41,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+` */
 
 setupCounter(document.querySelector('#counter'))
